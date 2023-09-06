069492927e29ca76d205584957b04bd48b95e068
Deni Kolomiets <deni0316@mail.ru>
2023-09-06 23:35:21 +0300

    Final 3 Rewiew 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import ru.yandex.javacource.Kolomiets.schedule.manager.TaskManager;
-import ru.yandex.javacource.Kolomiets.schedule.manager.tasks.Epic;
+package ru.yandex.javacource.kolomiets.schedule.tasks;
+
+import ru.yandex.javacource.kolomiets.schedule.manager.TaskManager;
 
 public class Main {
 
@@ -20,11 +21,11 @@ public class Main {
 
         Subtask sub11 = new Subtask("Title sub 1", "description sub 1", "NEW", epic1.getId());
         manager.addSub(sub11);
-        epic1.getSubIdArray().add(sub11.getId());
+        epic1.addSubtaskId(sub11.getId());
 
         Subtask sub12 = new Subtask("Title sub 12", "description sub 12", "NEW", epic1.getId());
         manager.addSub(sub12);
-        epic1.getSubIdArray().add(sub12.getId());
+        epic1.addSubtaskId(sub12.getId());
 
         Epic epic2 = new Epic("Title epic 2", "description epic 2", "NEW");
         manager.addEpic(epic2);
@@ -32,7 +33,7 @@ public class Main {
 
         Subtask sub21 = new Subtask("Title sub 21", "description sub 21", "NEW", epic2.getId());
         manager.addSub(sub21);
-        epic2.getSubIdArray().add(sub21.getId());
+        epic2.addSubtaskId(sub21.getId());
 
         Task newTask1 = new Task("Title newSimple 1", "description newSimple 1", "IN PROGRESS", simpleTask1.getId());
         manager.updateTask(newTask1);
@@ -43,10 +44,5 @@ public class Main {
 
         Subtask newSub12 = new Subtask("Title newSub 12", "description newSub 12", "DONE", epic1.getId());
         manager.updateSubtask(newSub12);
-
-        Epic newEpic1 = new Epic("Title newEpic 1", "description newEpic 1", epic1.getStatus(), epic1.getId());
-        manager.updateEpic(newEpic1);
-        System.out.println(newEpic1.getStatus());
-        System.out.println("Вывод саб айди " + newEpic1.getSubIdArray());
     }
 }
